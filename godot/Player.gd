7e6ea96ea7f46ce6baff01a023bb874d2dd6ce07
rlcomstock3 <rlcomstock3@gmail.com>
2021-12-31 13:32:05 -0700

    Added Bullets
    Bullets can now shoot with mouse click

--- godot/Player.gd
+++ godot/Player.gd
@@ -2,9 +2,14 @@ extends KinematicBody2D
 
 export (int) var speed = 150
 export (float) var rotation_speed = 1.5
+export (int) var bullet_speed = 300
 
 var velocity = Vector2()
 var rotation_dir = 0
+var can_fire = true
+var fire_delay = .3
+
+var bullet = preload("res://Bullet.tscn")
 
 func get_input():
 	rotation_dir = 0
@@ -17,6 +22,17 @@ func get_input():
 		velocity = Vector2(-speed, 0).rotated(rotation)
 	if Input.is_action_pressed("forward"):
 		velocity = Vector2(speed, 0).rotated(rotation)
+	if Input.is_action_pressed("fire") and can_fire:
+		var bullet_instance = bullet.instance()
+		print($Sprite/TankBody/Turret.global_rotation)
+		print(global_rotation)
+		bullet_instance.position = $Sprite/TankBody/Turret.get_global_position() + Vector2(70,0).rotated($Sprite/TankBody/Turret.global_rotation) 
+		bullet_instance.rotation = $Sprite/TankBody/Turret.global_rotation
+		bullet_instance.apply_impulse(Vector2(),Vector2(bullet_speed, 0).rotated($Sprite/TankBody/Turret.global_rotation))
+		get_tree().get_root().add_child(bullet_instance)
+		can_fire = false
+		yield(get_tree().create_timer(fire_delay), "timeout")
+		can_fire = true
 		
 	$Sprite/TankBody/Turret.look_at(get_global_mouse_position())
 
